9466fcdff70f4f7140af1b904f1ece69ee5406b6
raphael-f-santos <arvak1.format@gmail.com>
2026-07-17 20:36:01 -0300

    chore: configure docker and gradle settings

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.5" apply false
+    id("io.spring.dependency-management") version "1.1.4" apply false
+}
+
+allprojects {
+    group = "com.hashflow"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
